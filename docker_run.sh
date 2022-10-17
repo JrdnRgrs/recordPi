17347f4ab4b2dcc59727d0fefa8b1bd7638e343f
Jordan Rogers <jrdnrgrs@gmail.com>
2022-10-17 17:26:46 -0500

    add data.json output

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python3 record.py
-docker run --name "recordPi" --rm -v /recordings/turntable.mp3:/recordings/turntable.mp3 --env-file ./.env python-docker
+docker run --name "recordPi" --rm -v /recordings/turntable.mp3:/recordings/turntable.mp3 -v /media/Library/docker/apache/basic/data.json:/data.json --env-file ./.env python-docker
